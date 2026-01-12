349b011eb67080eefe5e33abf287a95b754dde9c
t s <shf@Mac-mini.local>
2026-01-12 22:51:44 +0900

    fix: use universal youtube.com host with origin param to resolve all playback issues

--- script.js
+++ script.js
@@ -371,16 +371,12 @@ class SyncPlayer {
         // If creating with empty ID, don't init player yet
         if (!videoId) return;
 
-        // Detect Mobile (iOS/Android)
-        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-        const host = isMobile ? 'https://www.youtube.com' : 'https://www.youtube-nocookie.com';
-
         this.player = new YT.Player(this.uniqueId, {
             height: '100%',
             width: '100%',
             videoId: videoId,
-            host: host,
-            playerVars: { 'playsinline': 1, 'rel': 0, 'autoplay': 0 },
+            host: 'https://www.youtube.com',
+            playerVars: { 'playsinline': 1, 'rel': 0, 'autoplay': 0, 'origin': window.location.origin },
             events: {
                 'onReady': (e) => this.onReady(e),
                 'onStateChange': (e) => this.onStateChange(e),
